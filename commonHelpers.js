6feae786c34b51cd22f064e25470168a0f0bdda5
viktormashyka <99292051+viktormashyka@users.noreply.github.com>
2023-11-17 11:20:36 +0000

    Deploying to gh-pages from @ viktormashyka/goit-advancedjs-hw03@ffbf1cd26b63e9a1491a18e3ea25f61936a350e6 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-import{a as d}from"./assets/vendor-26fe51b3.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const r of document.querySelectorAll('link[rel="modulepreload"]'))c(r);new MutationObserver(r=>{for(const s of r)if(s.type==="childList")for(const n of s.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&c(n)}).observe(document,{childList:!0,subtree:!0});function o(r){const s={};return r.integrity&&(s.integrity=r.integrity),r.referrerpolicy&&(s.referrerPolicy=r.referrerpolicy),r.crossorigin==="use-credentials"?s.credentials="include":r.crossorigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function c(r){if(r.ep)return;r.ep=!0;const s=o(r);fetch(r.href,s)}})();const b="live_ZRgbBHJqgNMula4MM0iCWTzXUjzZl2DkCaQ88DwD6Vsb5qyd5uxrl2YpDd75X6oV";d.defaults.baseURL="https://api.thecatapi.com/v1/";d.defaults.headers.common["x-api-key"]=b;async function B(){const e="breeds";try{return(await d.get(e)).data}catch(t){throw t.code==="ERR_BAD_REQUEST"&&console.log("Error","Bad request!"),new Error(t)}}async function T(e){const t=`images/search?breed_ids=${e}`;try{return(await d.get(t)).data}catch(o){throw o.code==="ERR_BAD_REQUEST"&&console.log("Error","Bad request!"),new Error(o)}}const h=document.querySelector(".breed-select"),i=document.querySelector(".loader"),a=document.querySelector(".error"),w=document.querySelector(".cat-info"),p="display: none;",l="display: block;";h.addEventListener("click",k);i.style.cssText=p;a.style.cssText=p;async function E(){try{i.style.cssText=l;const e=await B();return console.log("getBreeds: ",e),e}catch(e){console.log(e),a.style.cssText=l}finally{i.style.cssText=p}}E().then(e=>e.map(t=>{const o=`<option value=${t.id} key=${t.id}>${t.name}</option>`;h.insertAdjacentHTML("beforeend",o)}).join("")).catch(e=>{console.log(e),a.style.cssText=l});async function v(e){try{return i.style.cssText=l,await T(e)}catch(t){console.log(t),a.style.cssText=l}finally{i.style.cssText=p}}function k(e){e.preventDefault();const t=e.target.value;v(t).then(o=>{var r,s,n,u,y,f,g,x,m;console.log("Cat by breed: ",o[0]);const c=`<div style="display: flex; flex-direction: row">
+import{a as c}from"./assets/vendor-26fe51b3.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const r of document.querySelectorAll('link[rel="modulepreload"]'))a(r);new MutationObserver(r=>{for(const s of r)if(s.type==="childList")for(const p of s.addedNodes)p.tagName==="LINK"&&p.rel==="modulepreload"&&a(p)}).observe(document,{childList:!0,subtree:!0});function o(r){const s={};return r.integrity&&(s.integrity=r.integrity),r.referrerpolicy&&(s.referrerPolicy=r.referrerpolicy),r.crossorigin==="use-credentials"?s.credentials="include":r.crossorigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function a(r){if(r.ep)return;r.ep=!0;const s=o(r);fetch(r.href,s)}})();const u="live_ZRgbBHJqgNMula4MM0iCWTzXUjzZl2DkCaQ88DwD6Vsb5qyd5uxrl2YpDd75X6oV";c.defaults.baseURL="https://api.thecatapi.com/v1/";c.defaults.headers.common["x-api-key"]=u;async function f(){const e="breeds",{data:t}=await c.get(e);return t}async function m(e){const t=`images/search?breed_ids=${e}`,{data:o}=await c.get(t);return o}const n=document.querySelector(".breed-select"),l=document.querySelector(".loader"),d=document.querySelector(".error"),i=document.querySelector(".cat-info");n.addEventListener("change",y);async function h(){try{n.classList.add("hidden"),l.classList.remove("hidden");const e=await f();return console.log("getBreeds: ",e),e&&n.classList.remove("hidden"),e}catch(e){console.log(e),d.classList.remove("hidden")}finally{l.classList.add("hidden")}}h().then(e=>e.map(t=>{const o=`<option value=${t.id} key=${t.id}>${t.name}</option>`;n.insertAdjacentHTML("beforeend",o)}).join("")).catch(e=>{console.log(e),d.classList.remove("hidden")});async function g(e){try{n.classList.add("hidden"),i.classList.add("hidden"),d.classList.add("hidden"),l.classList.remove("hidden");const t=await m(e);return t&&(n.classList.remove("hidden"),i.classList.remove("hidden")),t}catch(t){console.log(t),d.classList.remove("hidden")}finally{l.classList.add("hidden")}}function y(e){e.preventDefault();const t=e.target.value;g(t).then(o=>{var r,s;i.innerHTML="",console.log("Cat by breed: ",o[0]);const a=`<div style="display: flex; flex-direction: row">
         <div
           style="
             margin-top: 30px;
@@ -13,8 +13,8 @@ import{a as d}from"./assets/vendor-26fe51b3.js";(function(){const t=document.cre
           "
         >
           <img
-            src=${(r=o[0])==null?void 0:r.url}
-            alt=${(n=(s=o[0])==null?void 0:s.breeds[0])==null?void 0:n.name}
+            src=${o[0].url}
+            alt=${o[0].breeds[0].name}
             style="
               display: block;
               width: 100%;
@@ -23,12 +23,12 @@ import{a as d}from"./assets/vendor-26fe51b3.js";(function(){const t=document.cre
         </div>
         <div style="display: block; margin-top: 30px">
           <h1 style="font-size: x-large; font-weight: 700; margin-bottom: 16px">
-            ${(y=(u=o[0])==null?void 0:u.breeds[0])==null?void 0:y.name}
+            ${o[0].breeds[0].name}
           </h1>
-          <p style="margin-bottom: 12px">${(g=(f=o[0])==null?void 0:f.breeds[0])==null?void 0:g.description}</p>
+          <p style="margin-bottom: 12px">${o[0].breeds[0].description}</p>
           <p>
-            <span style="font-weight: 700; padding-right: 8px">Temperament:</span>${(m=(x=o[0])==null?void 0:x.breeds[0])==null?void 0:m.temperament}
+            <span style="font-weight: 700; padding-right: 8px">Temperament:</span>${(s=(r=o[0])==null?void 0:r.breeds[0])==null?void 0:s.temperament}
           </p>
         </div>
-      </div>`;w.innerHTML=c}).catch(o=>{console.log(o),a.style.cssText=l})}
+      </div>`;o&&i.classList.remove("hidden"),i.innerHTML=a}).catch(o=>{console.log(o),d.classList.remove("hidden")})}
 //# sourceMappingURL=commonHelpers.js.map
